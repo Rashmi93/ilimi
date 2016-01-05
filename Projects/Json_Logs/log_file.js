b6c9734983db413eb683817e2d6ca5e90f6fc38f
rashmi93 <rashmi.n@upgradehr.co>
2016-01-05 11:07:13 +0530

    JSON LOG FILES

--- Projects/Json_Logs/log_file.js
+++ Projects/Json_Logs/log_file.js
@@ -1,33 +1,45 @@
 var _ = require('underscore');
+var fs=require('fs');
 var readLine = require('readline');
 var rl = readLine.createInterface({
-  input: require('fs').createReadStream('Test_Data.log')
-});
+   input : require('fs').createReadStream('Test_Data.log')
+   });
+var file = fs.createWriteStream('output.txt');
+    file.on('finish',function(){
+    console.log("file has been written");
+    });
 var events = [];
-
 // Get events
 rl.on('line', function(line) {
 	events.push(JSON.parse(line));
 });
 
 rl.on('close', function() {
+// Filter for OE_INTERACT
+    var oeEvents = _.where(events, {eid: 'OE_INTERACT'});
 
-	// Filter for OE_INTERACT
-
-	var oeEvents = _.where(events, {eid: 'OE_INTERACT'});
-
-    // get counts by uid & did using map
+    // get counts by uid & did using map 
 		uidMap = {};
 		didMap = {};
 		_.each(oeEvents, function(event){
-			if(uidMap[event.uid]){ uidMap[event.uid]++; }
+			if(uidMap[event.uid])
+			    { uidMap[event.uid]++; }
 			else
 			{ uidMap[event.uid]=1; }
-		    if(didMap[event.did]){ didMap[event.did]++;}
+		    if(didMap[event.did])
+			    { didMap[event.did]++;}
 		    else
 		    { didMap[event.did]=1; }
 		   
 		});
-		 console.log( "UserCount:" ,uidMap,"DeviceCount:",didMap);
-		
+	// converting uidmap to string
+		var uid_tostring=JSON.stringify(uidMap,null,'\n');
+		var uid_output = uid_tostring.replace(/\n/g, "\r\n");
+	//converting didmap to string
+		var did_tostring=JSON.stringify(didMap,null,'\n');
+		var did_output = did_tostring.replace(/\n/g, "\r\n");
+	//writing to output to file
+	    file.write(uid_output);
+	    file.write(did_output);
+	    file.end();
 });

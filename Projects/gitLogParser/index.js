feba040e34a578be9b7e6224bdba2c078be0d5ef
Rashmi N <Rashmi N>
2016-01-13 23:42:27 +0530

    gitLogParser

---
+++ Projects/gitLogParser/index.js
@@ -0,0 +1,9 @@
+var parseGitLog = require('./ex')
+var p = parseGitLog({cwd: 'C:/Users/rashmi n/ilimi/'})
+
+p.then(function (json) {
+  console.log(JSON.stringify(json, 0, 2))
+})
+.catch(function _catch (err) {
+  console.log('PROMISE ERR:', err)
+})

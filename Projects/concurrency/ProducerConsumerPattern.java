c1487f34e1fd0ada5946d42f86d5c51fbe47a3b7
rashmi93 <rashminrocks93@gmail.com>
2016-05-16 14:43:01 +0530

    ProducerConsumer

---
+++ Projects/concurrency/ProducerConsumerPattern.java
@@ -0,0 +1,73 @@
+package com.org.java.concurrency;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.io.*;
+
+public class ProducerConsumerPattern {
+	public static void main(String args[]) throws IOException {
+
+		// Creating shared object
+		BlockingQueue<String> sharedQueue = new LinkedBlockingQueue<String>();
+		ExecutorService pool, pool1;
+
+		// Creating Producer and Consumer Thread
+		pool = Executors.newFixedThreadPool(10);
+		pool1 = Executors.newFixedThreadPool(10);
+
+		for (int i = 0; i < 10; i++) {
+			pool.execute(new Consumer(sharedQueue));
+			pool1.execute(new Producer(sharedQueue));
+			i++;
+		}
+	}
+}
+
+// Producer Class in java
+class Producer implements Runnable {
+
+	private final BlockingQueue<String> sharedQueue;
+	public Producer(BlockingQueue<String> sharedQueue2) {
+		this.sharedQueue = sharedQueue2;
+	}
+
+	@Override
+	public void run() {
+		try {
+			File f = new File("files/testData");
+			FileReader in = new FileReader(f);
+			BufferedReader br = new BufferedReader(in);
+			String strLine;
+
+			while ((strLine = br.readLine()) != null) {
+				System.out.println("Produced: " + strLine);
+				sharedQueue.put(strLine);
+			}
+			// System.out.println(strLine);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+
+// Consumer Class in Java
+class Consumer implements Runnable {
+
+	private final BlockingQueue<String> sharedQueue;
+	public Consumer(BlockingQueue<String> sharedQueue2) {
+		this.sharedQueue = sharedQueue2;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				System.out.println("Consumed: " + sharedQueue.take());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
